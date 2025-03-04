384d57d5b413a833cadc8679c49c7562137d42ce
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2025-03-04 02:05:54 -0300

    Update botonMostrarTicket.js

--- public/js/botonMostrarTicket.js
+++ public/js/botonMostrarTicket.js
@@ -2,19 +2,19 @@ import { generarTicket } from "./generarTicket.js";
 import { validarCobro } from "./validarCobro.js";
 
 export function botonTicket() {
-  const $botonTicket = document.createElement("button");
-  $botonTicket.className = "botonTicket";
-  $botonTicket.innerHTML = "Hacer<br>Ticket";
-  $botonTicket.addEventListener("click", (e) => {
+	const $botonTicket = document.createElement("button");
+	$botonTicket.className = "botonTicket";
+	$botonTicket.innerHTML = "Hacer<br>Ticket";
+	$botonTicket.addEventListener("click", (e) => {
     if(document.querySelector("#mesaMostrada").innerHTML != "Mesa"){
-    document.querySelector(".mesasAbiertas").style.display= "none"
-	 validarCobro()
-    generarTicket()
-    document.querySelector(".ticket").style.display= "flex"}
-  	}else{
+		if(!document.querySelector(".art")){return window.alert("No hay articulos que cobrar")}
+    	document.querySelector(".mesasAbiertas").style.display= "none"
+		validarCobro()
+    	generarTicket()
+    	document.querySelector(".ticket").style.display= "flex"
+	}else{
   		return window.alert("No hay mesa seleccionada")
   	}
-	if(!document.querySelector(".art")){return window.alert("No hay articulos que cobrar"}
 	
 	);
   return $botonTicket;
