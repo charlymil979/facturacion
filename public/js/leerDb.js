a7bbad598d8a447f41cd74250dfe371209887ee3
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2025-03-03 22:00:00 -0300

    Update leerDb.js

--- public/js/leerDb.js
+++ public/js/leerDb.js
@@ -1,13 +1,21 @@
 
 // Función para cargar el archivo JSON
-export default async function datosDb() {
+export async function dbMesas() {
   try {
-    const response = await fetch("http://localhost:3000/json/db.json")
-    const data = await response.json()
-    return data
+    const data = await fetch("http://localhost:3000/mesas")
+    const mesas = await data.json()
+    return mesas
   } catch (error) {
     console.log("error: ",error)
   }
-
-  }
+}
   
+export async function dbPrecios() {
+  try {
+    const data = await fetch("http://localhost:3000/datos")
+    const precios = await data.json()
+    return precios
+  } catch (error) {
+    console.log("error: ",error)
+  }
+}
