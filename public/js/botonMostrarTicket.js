80f3021dd3c8978ebe41696e39fd50504e363678
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2025-03-06 16:58:17 -0300

    Update botonMostrarTicket.js

--- public/js/botonMostrarTicket.js
+++ public/js/botonMostrarTicket.js
@@ -6,23 +6,25 @@ export function botonTicket() {
 	$botonTicket.className = "botonTicket";
 	$botonTicket.innerHTML = "Hacer<br>Ticket";
 	$botonTicket.addEventListener("click", (e) => {
-		let fecha=""
-		const data = JSON.parse(localStorage.getItem("cajaInicial"));
+	let fecha=""
+	const data = JSON.parse(localStorage.getItem("cajaInicial"));
     if(data){fecha=data.Fecha}
-	 if(fecha === ""){return window.alert("Abrir caja, seleccione hora de apertura")}
-    if(document.querySelector("#mesaMostrada").innerHTML != "Mesa"){
-		if(!document.querySelector(".art")){
-			return window.alert("No hay articulos que cobrar")
-		}
-    	document.querySelector(".mesasAbiertas").style.display= "none"
-		validarCobro()
-    	generarTicket()
-		document.querySelector(".anularFondo").style.display = "flex";
-    	document.querySelector(".ticket").style.display= "flex"
-	}else{
-  		return window.alert("No hay mesa seleccionada")
-  	}
+	console.log(fecha)
+	if(fecha != ""){
+    	if(document.querySelector("#mesaMostrada").innerHTML != "Mesa"){
+			if(!document.querySelector(".art")){
+				return window.alert("No hay articulos que cobrar")
+			}
+    		document.querySelector(".mesasAbiertas").style.display= "none"
+			validarCobro()
+    		generarTicket()
+			document.querySelector(".anularFondo").style.display = "flex";
+    		document.querySelector(".ticket").style.display= "flex"
+		}else{
+  			return window.alert("No hay mesa seleccionada")
+  		}
 	
-	});
+		}else{
+		return window.alert("Abrir caja, seleccione hora de apertura")});
   return $botonTicket;
 }
