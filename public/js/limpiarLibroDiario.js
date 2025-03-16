4baafaa4c9a5026d38ef19b4b543d216a7f6fd3d
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2025-03-16 18:55:11 -0300

    Add files via upload

--- public/js/limpiarLibroDiario.js
+++ public/js/limpiarLibroDiario.js
@@ -1,20 +1,22 @@
-
 export function limpiarLibroDiario() {
-	document.querySelectorAll(".ini").forEach(el =>{
-		el.value = ""
-	});
-	localStorage.setItem("cajaInicial","{}");
-	const inicio = document.querySelectorAll(".inicioCaja")
-	inicio.forEach(el => el.value="")
-	const totales = document.querySelectorAll(".itemIngreso");
-	totales.forEach(el => el.value="")
-	localStorage.setItem("egresosDiario", "[]");
-	const tabla = document.querySelector(".tablaResumen")
-	const td = tabla.querySelectorAll("td")
-	const nombres = [0,5,10,15,20]
-	td.forEach((el,n)=>{
-		if(!nombres.includes(n)){
-			el.innerText = ""
-		}
-	})
+	const inicio = document.querySelectorAll(".inicioCaja");
+	inicio[0].value = "";
+	inicio[1].value = document.querySelector("#totEf").innerHTML;
+	inicio[2].value = document.querySelector("#totCta").innerHTML;
+  document.querySelectorAll(".ini").forEach((el) => {
+    el.value = "";
+  });
+  localStorage.setItem("cajaInicial", "{}");
+//   inicio.forEach((el) => (el.value = ""));
+  const totales = document.querySelectorAll(".itemIngreso");
+  totales.forEach((el) => (el.value = ""));
+  localStorage.setItem("egresosDiario", "[]");
+  const tabla = document.querySelector(".tablaResumen");
+  const td = tabla.querySelectorAll("td");
+  const nombres = [0, 5, 10, 15, 20];
+  td.forEach((el, n) => {
+    if (!nombres.includes(n)) {
+      el.innerText = "";
+    }
+  });
 }
